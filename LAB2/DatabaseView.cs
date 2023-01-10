c80ac0e1eb91108ac6c3ab93774174a6c4d47507
AndriyOdnoraz <115658538+AndriyOdnoraz@users.noreply.github.com>
2023-01-10 14:02:50 +0200

    Add lab2

---
+++ LAB2/DatabaseView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreConsoleInteractorCS
+{
+    public class DatabaseView
+    {
+        DatabaseController controller;
+        
+        public void Process()
+        {
+            Console.WriteLine("Input connection string >> ");
+            string connection_string = Console.ReadLine();
+            controller = new DatabaseController(connection_string);
+            while (true)
+            {
+                int action = controller.ShowActionList();
+                controller.Execute(action);
+            }
+        }
+    }
+}
